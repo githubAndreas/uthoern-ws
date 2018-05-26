1477c575a641078ccb3c5753672783dd4a49dab0
Andreas Huebner <Andyhuebie@web.de>
2018-05-26 15:50:04 +0200

    Add PlaylistUtil.count_playlists_of_slices(...)

---
+++ playlist_util.py
@@ -0,0 +1,22 @@
+from logger import Logger
+
+
+class PlaylistUtil:
+
+    @staticmethod
+    def count_playlists_of_slices(playlist_slices):
+        Logger.log_info('Start counting total number of playlists')
+
+        greatest_id = -1;
+
+        for p_slice in playlist_slices:
+
+            for playlist in p_slice.get_playlist_collection():
+                pid = playlist.get_pid()
+                if greatest_id < pid:
+                    greatest_id = pid
+
+        Logger.log_info('Slice[' + p_slice.get_info().get_item_range() + '] successfully inspected')
+
+        Logger.log_info(str(greatest_id) + ' playlists totally founded')
+        return greatest_id
