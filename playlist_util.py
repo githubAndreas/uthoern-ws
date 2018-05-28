11575cc60ffa0c6830c19ddeb71b1037507d4879
Andreas Huebner <Andyhuebie@web.de>
2018-05-28 12:09:14 +0200

    Add total track counter

--- playlist_util.py
+++ playlist_util.py
@@ -9,16 +9,13 @@ class PlaylistUtil:
     def count_playlists_of_slices(playlist_slices: List[PlaylistSlice]) -> int:
         Logger.log_info('Start counting total number of playlists')
 
-        greatest_id = -1;
+        counter = 0;
 
         for p_slice in playlist_slices:
-
             for playlist in p_slice.get_playlist_collection():
-                pid = playlist.get_pid()
-                if greatest_id < pid:
-                    greatest_id = pid
+                counter = counter + 1
 
-        Logger.log_info('Slice[' + p_slice.get_info().get_item_range() + '] successfully inspected')
+            Logger.log_info('Slice[' + p_slice.get_info().get_item_range() + '] successfully inspected')
 
-        Logger.log_info(str(greatest_id) + ' playlists totally founded')
-        return greatest_id
+        Logger.log_info(str(counter) + ' playlists totally founded')
+        return counter
