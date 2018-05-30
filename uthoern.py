50feaa340176cacd27b36ec63bca3b82a84c9a01
Andreas Huebner <Andyhuebie@web.de>
2018-05-30 15:45:04 +0200

    Replace cell values with calculated values

--- uthoern.py
+++ uthoern.py
@@ -5,6 +5,7 @@ from ranging_matrix_factory import RangingMatrixFactory
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.svm import SVR
+import numpy as np
 from sklearn.model_selection import train_test_split
 
 if __name__ == '__main__':
@@ -17,18 +18,20 @@ if __name__ == '__main__':
     ranging_df = RangingMatrixFactory.create_data_frame(slices)
     template_ranging_matrix = RangingMatrixFactory.create_template_ranging_matrix(ranging_df)
 
-    for index, target_column in enumerate(ranging_df.columns):
-        Logger.log_info('Model[' + str(index + 1) + '/' + str(len(ranging_df.columns)) + '] Name:' + target_column )
+    for column_index, target_column in enumerate(ranging_df.columns):
+        Logger.log_info(
+            'Model[' + str(column_index + 1) + '/' + str(len(ranging_df.columns)) + '] Name:' + target_column)
 
         # Testdaten
         y = ranging_df[target_column]
         X = ranging_df.drop(target_column, 'columns').as_matrix()
+
         X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
         # Modele
         # reg = KNeighborsRegressor(n_neighbors=2,n_jobs=-1)
-        reg = LinearRegression(n_jobs=-1)
-        # reg = Ridge()
+        # reg = LinearRegression(n_jobs=-1)
+        reg = Ridge()
         # reg = Lasso
         # reg = SVR(C=1.0, epsilon=0.2)
 
@@ -37,7 +40,15 @@ if __name__ == '__main__':
         print("Score Trainingsdatensatz: {:.2f}".format(reg.score(X_train, y_train)))
 
         # vorhersage
-        prediction_vector = reg.predict(X_test)
+        predicted_column = reg.predict(X_test)
+
+        new_column = np.append(y_train, predicted_column)
+
+        for row_index, row in ranging_df.iterrows():
+            cell_value = row[target_column]
+            if cell_value != 1:
+                cell_value = new_column[row_index]
+
         print("Score Testdatensatz: {:.2f}".format(reg.score(X_test, y_test)))
 
     Logger.log_info('Stop uthoern')
