f733e6ad70ae8a2941938ff9b55f8cc6e5cb2038
Andreas Huebner <Andyhuebie@web.de>
2018-06-02 11:27:25 +0200

    Remove unused attributes from track

--- track.py
+++ track.py
@@ -1,42 +1,17 @@
-from album import Album
-from artist import Artist
 from typing import Dict
 
 
 class Track:
 
-    def __init__(self, name: str, uri: str, pos: int, artist: str, album: str, duration: str):
-        self.__name = name
+    def __init__(self, uri: str):
         self.__uri = uri
-        self.__pos = pos
-        self.__artist = artist
-        self.__album = album
-        self.__duration = duration
-
-    def get_name(self) -> str:
-        return self.__name
 
     def get_uri(self) -> str:
         return self.__uri
 
-    def get_simplified_uri(self)-> str:
+    def get_simplified_uri(self) -> str:
         return self.__uri[14:]
-
-    def get_pos(self) -> int:
-        return self.__pos
-
-    def get_artist(self) -> str:
-        return self.__artist
-
-    def get_album(self) -> str:
-        return self.__album
-
-    def get_duration(self) -> str:
-        return self.__duration
 
     @staticmethod
     def from_dict(data: Dict) -> 'Track':
-        album = Album.from_dict(data)
-        artist = Artist.from_dict(data)
-
-        return Track(data['track_name'], data['track_uri'], int(data['pos']), artist, album, data['duration_ms'])
+        return Track(data['track_uri'])
