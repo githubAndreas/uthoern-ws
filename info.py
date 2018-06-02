fe601e17480b54656b733ae9b629e2f97faf4e67
Andreas Huebner <Andyhuebie@web.de>
2018-06-02 11:17:10 +0200

    Remove unused attributes

--- info.py
+++ info.py
@@ -3,20 +3,12 @@ from typing import Dict
 
 class Info:
 
-    def __init__(self, generated_on: str, item_range: str, version: str):
-        self.__generated_on = generated_on
+    def __init__(self, item_range: str):
         self.__item_range = item_range
-        self.__version = version
-
-    def get_generatd_on(self) -> str:
-        return self.__generated_on
 
     def get_item_range(self) -> str:
         return self.__item_range
 
-    def get_version(self) -> str:
-        return self.__version
-
     @staticmethod
     def from_dict(data: Dict) -> 'Info':
-        return Info(data['generated_on'], data['slice'], data['version'])
+        return Info(data['slice'])
