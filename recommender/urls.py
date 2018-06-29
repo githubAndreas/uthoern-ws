18aa80bb509043da9189a28bc5b6e04add9c8aed
Andreas Huebner <Andyhuebie@web.de>
2018-06-29 11:01:35 +0200

    DJANGO > Add default url

--- recommender/urls.py
+++ recommender/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 
 from . import views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^dataset$', views.dataset, name='dataset'),
     url(r'^training$', views.training, name='training'),
     url(r'^prediction$', views.prediction, name='prediction'),
     url(r'^prediction/(?P<prediction_id>\d+)/$$', views.download_prediction_csv, name='prediction'),
+
+    url(r'^.*$', RedirectView.as_view(url="/dataset"), name='default_redirect'),  # default route
 ]
