9af2b4a5b9ae456e84b5ff470ed10f64e0552b5f
Andreas Huebner <Andreas-huebner@live.de>
2018-06-30 20:33:40 +0200

    Update readme

--- recommender/admin.py
+++ recommender/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Environment, Decomposition
+from .models import Environment, Decomposition, Model_Algorithm
 
 
 # Register your models here.
 admin.site.register(Environment)
 admin.site.register(Decomposition)
+admin.site.register(Model_Algorithm)
