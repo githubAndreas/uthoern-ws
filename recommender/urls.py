f839c973ed97179d46790120dd3a005e228fe649
Andreas Huebner <Andreas-huebner@live.de>
2018-07-10 13:34:48 +0200

    Bugfix: Refactoring of url configuration

--- recommender/urls.py
+++ recommender/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^dataset$', views.dataset, name='dataset'),
     url(r'^training$', views.training, name='training'),
     url(r'^prediction$', views.prediction, name='prediction'),
-    url(r'^prediction/(?P<prediction_id>\d+)/$$', views.download_prediction_csv, name='prediction'),
+    url(r'^prediction/(?P<prediction_id>\d+)$', views.download_prediction_csv, name='prediction'),
 
     url(r'^.*$', RedirectView.as_view(url="/dataset"), name='default_redirect'),  # default route
 ]
